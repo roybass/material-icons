d38f860783cc018c8f053dd5c2d7e6a1ba3cad2c
obroybass <rbass@outbrain.com>
2018-12-10 12:56:36 +0200

    commiting build, to display on github pages

---
+++ build/precache-manifest.004e89b142cf7fbdf4b80c312b1e2c50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "929973c71e55ef9c8a6b",
+    "url": "/static/js/main.929973c7.chunk.js"
+  },
+  {
+    "revision": "6da09467639d11af632a",
+    "url": "/static/js/1.6da09467.chunk.js"
+  },
+  {
+    "revision": "929973c71e55ef9c8a6b",
+    "url": "/static/css/main.c4bca606.chunk.css"
+  },
+  {
+    "revision": "aa2d0c91546d0e484bdc39ffd2d9115d",
+    "url": "/index.html"
+  }
+];
